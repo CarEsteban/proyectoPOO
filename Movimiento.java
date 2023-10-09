d1981f6785ddc7b64a8d95854665801f7bcc4b42
Nicolás <con23197@uvg.edu.gt>
2023-10-08 22:22:12 -0600

    Clase de movimiento terminada

--- Movimiento.java
+++ Movimiento.java
@@ -1,3 +1,20 @@
 public class Movimiento {
-    
+    private int monto;
+    private String categoria;
+    private String descripcion;
+    private int tipo; 
+
+    //setters
+    public int getMonto() {
+        return monto;
+    }
+    public String getCategoria() {
+        return categoria;
+    }
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public int getTipo() {
+        return tipo;
+    }
 }
