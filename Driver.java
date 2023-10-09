fcb75075a7337d52d596e4a5cdb863b3feeddc49
CarEsteban <car23016@uvg.edu.gt>
2023-10-08 23:28:08 -0600

    Arreglo en la identificacion del usuario

--- Driver.java
+++ Driver.java
@@ -5,23 +5,38 @@ public class Driver {
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int salir = 1;
-        int opcion = 0;
-        // String usuario = "";
-        // String contrasenia = "";
-        
-        // boolean loggedIn = false;
-        // while (!loggedIn) {
-        //     System.out.print("Ingrese su usuario: ");
-        //     usuario = scanner.nextLine();
-        //     System.out.print("Ingrese su contraseña: ");
-        //     contrasenia = scanner.nextLine();
-        //     loggedIn = autenticarUsuario(usuario, contrasenia);
-            
-        //     if (!loggedIn) {
-        //         System.out.println("Usuario o contraseña incorrectos. Intente nuevamente.");
-        //     }
-        // }
+        int salir = 1, opcion = 0,opc;
+        String usuario, contrasenia ;
+        Usuario user = null; 
+
+        boolean loggedIn = false;
+        while (!loggedIn) {
+            System.out.print("Ingrese su usuario: \n");
+            usuario = scanner.nextLine();
+            System.out.print("Ingrese su contraseña: \n");
+            contrasenia = scanner.nextLine();
+
+            if (user != null) {
+                loggedIn = autenticarUsuario(user);
+            }
+
+            if (!loggedIn) {
+                System.out.println("Usuario o contraseña incorrectos. Intente nuevamente.");
+                System.out.print("Desea crear un usuario? 1)Si 2)No\n" );
+                opc = scanner.nextInt();
+                scanner.nextLine();
+                switch (opc) {
+                    case 1: 
+                        String[] datosUsuario = ingresarNuevoUsuario();
+                        user = new Usuario(datosUsuario[0], datosUsuario[1], datosUsuario[2], datosUsuario[3], datosUsuario[4], datosUsuario[5], null, null);
+                        break;
+                    case 2:
+                        loggedIn = false;
+                        break;
+                }
+            }
+        }
+
 
         while (salir != 0) {
             printMenu();
@@ -67,12 +82,38 @@ public class Driver {
         }
     }
 
-        // public static boolean autenticarUsuario(String usuario, String contrasenia) {
-    //     String usuarioValido = "usuario_valido";
-    //     String contraseniaValida = "contrasenia_valida";
-    //
-    //     return usuario.equals("usuario_valido") && contrasenia.equals("contrasenia_valida");
-    // }
+    public static String[] ingresarNuevoUsuario(){
+        String nombre, apellido, fechaNac, dpi, correo, contrasenia;
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Ingrese su nombre: ");
+        nombre = scanner.nextLine();
+        
+        System.out.print("Ingrese su apellido: ");
+        apellido = scanner.nextLine();
+        
+        System.out.print("Ingrese su fecha de nacimiento: ");
+        fechaNac = scanner.nextLine();
+        
+        System.out.print("Ingrese su número de DPI: ");
+        dpi = scanner.nextLine();
+        
+        System.out.print("Ingrese su correo electrónico: ");
+        correo = scanner.nextLine();
+        
+        System.out.print("Ingrese su contraseña: ");
+        contrasenia = scanner.nextLine();
+
+        
+        String[] datosUsuario = {nombre, apellido, fechaNac, dpi, correo, contrasenia};
+        return datosUsuario;
+    }
+
+    public static boolean autenticarUsuario(Usuario user) {
+
+        return user.getNombre().equals(user.getNombre()) && user.getContrasenia().equals(user.getContrasenia());
+    }
 
     public static void printMenu() {
         System.out.println("");
@@ -85,9 +126,11 @@ public class Driver {
         System.out.println("2: Gastos");
         System.out.println("3: Ingresos");
         System.out.println("4: Balance");
-        System.out.println("5: Consejos");
+        System.out.println("5: Consejos extras");
         System.out.println("6: Consulta de saldo por fechas");
         System.out.println("7: Salir");
+        System.out.println("");
+        System.out.println("");
         System.out.println("");
     }
 }
